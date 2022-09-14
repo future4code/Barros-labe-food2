e833d1ab349019d6bd55c76e292b41ff81a26c86
Maria Fernandez de Moura Ferro <mariafmferro@gmail.com>
2022-09-14 20:29:46 -0300

    Criei o componente botão

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+import { ButtonStyle } from "./styled"
+
+
+export const Button = ( {buttonTitle} ) => {
+
+    return (
+        <ButtonStyle><p>{buttonTitle}</p></ButtonStyle>
+    )
+
+}
---
+++ src/components/Button/styled.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const ButtonStyle = styled.button`
+    width: 328px;
+    height: 42px;
+    background-color: var(--mid-green);
+    padding: 12px 16px;
+    border-radius: 2px;
+    border: none;
+    transition: 0.2s;
+
+    p {
+        width: 296px;
+        height: 18px;
+        text-align: center;
+        color: var(--black);
+    }
+
+    :active {
+        transform: scale(0.98)
+    }
+`
