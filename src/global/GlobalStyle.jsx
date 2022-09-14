1ae6a9a99ccea233ec918c7f40197d1067cc5ceb
Maria Fernandez de Moura Ferro <mariafmferro@gmail.com>
2022-09-14 11:31:32 -0300

    Adicionei os estilos globais de fonte e um root de cores, conforme documentação

--- src/global/GlobalStyle.jsx
+++ src/global/GlobalStyle.jsx
@@ -6,5 +6,14 @@ export const GlobalStyle = createGlobalStyle `
         padding: 0;
         margin: 0;
         box-sizing: border-box;
+        font-family: 'Roboto', sans-serif;
+        font-size: 16px;
+        letter-spacing: -0.4px;
+    }
+
+    :root {
+        --black: #000000;
+        --greyish: #B8B8B8;
+        --mid-green: #5CB646;
     }
 `
