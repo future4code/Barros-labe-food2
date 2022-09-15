90f52a1616bb83c538ac0b3192cde776423a1d46
Maria Fernandez de Moura Ferro <mariafmferro@gmail.com>
2022-09-15 11:52:08 -0300

    Adicionei os nomes nos inputs

--- src/components/Inputs/Email.jsx
+++ src/components/Inputs/Email.jsx
@@ -5,7 +5,7 @@ export const Email = ( { value, onChange } ) => {
     return (
         <InputStyle>
         <label>E-mail*</label>
-        <input value={value} onChange={onChange} placeholder="email@email.com" type="e-mail" />
+        <input name="email" value={value} onChange={onChange} placeholder="email@email.com" type="e-mail" />
         </InputStyle>
     )
 }
--- src/components/Inputs/Password.jsx
+++ src/components/Inputs/Password.jsx
@@ -12,7 +12,7 @@ export const Password = ( { value, onChange } ) => {
         <InputPassword>
             <label>Senha*</label>
             <div>
-                <input value={value} onChange={onChange} placeholder="Mínimo 6 caracteres" type={showPassword ? "text" : "password"} />
+                <input name="password" value={value} onChange={onChange} placeholder="Mínimo 6 caracteres" type={showPassword ? "text" : "password"} />
                 <button onClick={clickShowPassword}>
                     {showPassword ? <img src={password2} alt="Mostrar senha"/> : <img src={password1} alt="Esconder senha"/>}
                 </button>
