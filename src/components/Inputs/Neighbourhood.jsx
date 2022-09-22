ea3fb0ba6669425ab321c3283be326abea0184f9
Samuel <samuca010@gmail.com>
2022-09-21 22:13:44 -0300

    Inclusão de requisição de Editar Endereço e Editar Perfil

--- src/components/Inputs/Neighbourhood.jsx
+++ src/components/Inputs/Neighbourhood.jsx
@@ -1,11 +1,11 @@
 import { InputStyle } from "./styled"
 
-export const District = ( { value, onChange, color } ) => {
+export const Neighbourhood = ( { value, onChange, color } ) => {
 
     return (
         <InputStyle color={color}>
         <label>Bairro*</label>
-        <input name="district" value={value} onChange={onChange} placeholder="Bairro" type="text" 
+        <input name="neighbourhood" value={value} onChange={onChange} placeholder="Bairro" type="text" 
         pattern="^[a-zA-Z][a-zA-Z-_\.]{3,20}$" required
         title="Digite o nome com pelo menos 3 carateres"/>
         </InputStyle>
--- src/pages/EditAddressPage/EditAddressPage.jsx
+++ src/pages/EditAddressPage/EditAddressPage.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import {Header} from '../../components/Header/Header'
 import { City } from "../../components/Inputs/City";
 import { Complement } from "../../components/Inputs/Complement";
-import { District } from "../../components/Inputs/District";
+import { Neighbourhood } from "../../components/Inputs/Neighbourhood";
 import { Number } from "../../components/Inputs/Number";
 import { State } from "../../components/Inputs/State";
 import { Street } from "../../components/Inputs/Street";
@@ -11,9 +11,7 @@ import { Button } from "../../components/Button/Button";
 import { useForm } from "../../hooks/useForm";
 import axios from "axios";
 import { BASE_URL } from "../../constants/constants";
-import * as MyRoutes from "../../routes/coordinator";
 import { useNavigate } from "react-router-dom";
-// import { validateCPF, validateEmail, validateName } from "../../constants/constants";
 
 const EditAddressPage = () => {
 
@@ -27,13 +25,13 @@ const EditAddressPage = () => {
 
     const [isValid, setIsValid] = useState(true)
     const [isStreetValid, setIsStreetValid, isNumberValid, setIsNumberValid,
-           isComplementValid, setIsComplementValid, isDistrictValid, setIsDistrictValid,
+           isComplementValid, setIsComplementValid, isNeighbourhoodValid, setIsNeighbourhoodValid,
            isCityValid, setIsCityValid, isStateValid, setIsStateValid  ] = useState(true)
     
     const [errorText, setErrorText] = useState(undefined)
 
     const EditAddress = () => {
-        axios.post(`${BASE_URL}/signup`, form)
+        axios.put(`${BASE_URL}/address`, form)
         .then((response) => {
             localStorage.setItem("token", response.token)
             goToProfilePage(navigate)
@@ -48,10 +46,10 @@ const EditAddressPage = () => {
         e.preventDefault();
         setIsStreetValid(validateStreet(form.street))
         setIsNumberValid(validateNumber(form.number))
-        setIsComplementValid(validateComplement(form.complement))
-        setIsDistrictValid(validateDistrict(form.district))
+        setIsNeighbourhoodValid(validateNeighbourhood(form.neighbourhood))
         setIsCityValid(validateCity(form.city))
-        setIsStateValid(validateState(form.State))
+        setIsStateValid(validateState(form.state))
+        setIsComplementValid(validateComplement(form.complement))
     }
     return(
         <>
@@ -63,7 +61,7 @@ const EditAddressPage = () => {
                 <Street name="street" value={form.street} onChange={onChange} color="#B8B8B8" isValid={isStreetValid}/>
                 <Number name="number" value={form.number} onChange={onChange} color="#B8B8B8" isValid={isNumberValid}/>
                 <Complement name="complement" value={form.complement} onChange={onChange} color="#B8B8B8" isValid={isComplementValid}/>
-                <District name="district" value={form.district} onChange={onChange} color="#B8B8B8" isValid={isDistrictValid}/>
+                <Neighbourhood name="neighbourhood" value={form.neighbourhood} onChange={onChange} color="#B8B8B8" isValid={isNeighbourhoodValid}/>
                 <City name="city" value={form.city} onChange={onChange} color="#B8B8B8" isValid={isCityValid}/>
                 <State name="state" value={form.state} onChange={onChange} color="#B8B8B8" isValid={isStateValid}/>
                 <Button type="submit" buttonTitle="Salvar"/>
@@ -75,14 +73,12 @@ const EditAddressPage = () => {
                 <Street name="street" value={form.street} onChange={onChange} color="#e02020" isValid={isStreetValid}/>
                 <Number name="number" value={form.number} onChange={onChange} color="#e02020" isValid={isNumberValid}/>
                 <Complement name="complement" value={form.complement} onChange={onChange} color="#e02020"isValid={isComplementValid}/>
-                <District name="district" value={form.district} onChange={onChange} color="#e02020" isValid={isDistrictValid}/>
+                <Neighbourhood name="neighbourhood" value={form.neighbourhood} onChange={onChange} color="#e02020" isValid={isNeighbourhoodValid}/>
                 <City name="city" value={form.city} onChange={onChange} color="#e02020" isValid={isCityValid}/>
                 <State name="state" value={form.state} onChange={onChange} color="#e02020" isValid={isStateValid}/>
                 <p>{errorText}.</p>
                 <Button type="submit" buttonTitle="Salvar"/>
                 </form>}
-
-
         </AddressPageStyle>
         </>
     )
