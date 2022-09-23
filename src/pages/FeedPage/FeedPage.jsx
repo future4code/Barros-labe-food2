b2c6da9fc955f598291c5d5d97588c83e44ed577
Byron Belizario Smith Nobrega <byronnobrega@hotmail.com>
2022-09-23 06:31:32 -0300

    Conflitos na page FeedPage

--- src/pages/FeedPage/FeedPage.jsx
+++ src/pages/FeedPage/FeedPage.jsx
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-import React, { useContext, useState } from "react";
-import {Header} from '../../components/Header/Header'
-import {ButtonSearch, FeedPageStyle, FiltersContainer, CardsContainer} from './style'
-=======
 import React, { useContext, useEffect, useState } from "react";
 import {Header} from '../../components/Header/Header'
 import {ButtonSearch, FeedPageStyle, FiltersContainer} from './style'
->>>>>>> byron-Restaurants-4
 import search from '../../images/search.png'
 import {Loading} from '../../components/Loading/Loading'
 import GlobalContext from '../../context/GlobalContext'
@@ -20,21 +14,12 @@ const FeedPage = () => {
 
     const {dataRestaurants, errorRestaurants, isLoadingRestaurants} = useContext(GlobalContext)
     const {showOrder, setShowOrder} = useContext(GlobalContext)
-<<<<<<< HEAD
-    const [category, setCategory] = useState("")
-=======
     const [category, setCategory] = useState("Hamburguer")
->>>>>>> byron-Restaurants-4
     const navigate = useNavigate()    
 
     const restaurantsList = dataRestaurants && dataRestaurants.restaurants.map((restaurant) =>{
         if(restaurant.category === category){
         return <RestaurantButtonCard restaurant={restaurant} key={restaurant.id}/>            
-<<<<<<< HEAD
-        } else if(category === ""){
-            return <RestaurantButtonCard restaurant={restaurant} key={restaurant.id}/>   
-=======
->>>>>>> byron-Restaurants-4
         }
     })
     
@@ -57,21 +42,7 @@ const FeedPage = () => {
                 <button onClick={()=>{setCategory("Petiscos")}}>Petiscos</button>
                 <button onClick={()=>{setCategory("Mexicana")}}>Mexicana</button>               
 
-<<<<<<< HEAD
-            </FiltersContainer>   
 
-            <CardsContainer>
-                
-                {isLoadingRestaurants && <Loading/>}
-
-                {!isLoadingRestaurants&&errorRestaurants&&<p>{errorRestaurants}</p>}
-
-                {!isLoadingRestaurants&&dataRestaurants&&restaurantsList}
-
-                {localStorage.getItem("orderInProgress")==="true" && <Order/>}
-                
-            </CardsContainer>               
-=======
             </FiltersContainer>           
         
             {isLoadingRestaurants && <Loading/>}
@@ -81,7 +52,6 @@ const FeedPage = () => {
             {!isLoadingRestaurants&&dataRestaurants&&restaurantsList}
 
             {localStorage.getItem("orderInProgress")==="true" && <Order/>}
->>>>>>> byron-Restaurants-4
 
             <Footer color1={'#5CB646'} color2={'#B8B8B8'} color3={'#B8B8B8'}/>                  
 
